581b8b313e42d292521a019fddd0419a1a1c21a6
Amiran <chicobes1@gmail.com>
2022-02-10 23:02:37 +0300

    do decomposition

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,16 @@
 <template>
-  <div>
-    <form @submit.prevent>
-      <h4>Создание поста</h4>
-      <input
-        v-bind:value="title"
-				@input="title = $event.target.value"
-        class="input"
-        type="text"
-        placeholder="Название"
-      />
-      <input
-        v-bind:value="body"
-				@input="body = $event.target.value"
-        class="input"
-        type="text"
-        placeholder="Описание"
-      />
-      <button @click="createPost">Создать</button>
-      <button @click="removePost">Удалить</button>
-    </form>
-    <div class="post" v-for="post in posts">
-      <div><strong>Название:</strong> {{ post.title }}</div>
-      <div><strong>Описание:</strong> {{ post.body }}</div>
-    </div>
+  <div class="app">
+    <post-form />
+    <post-list :posts="posts"/>
   </div>
 </template>
 <script>
+import PostForm from "@/components/postForm";
+import PostList from "@/components/postList";
 export default {
+  components:{
+    PostForm, PostList
+  },
   data() {
     return {
       posts: [
@@ -65,41 +49,4 @@ export default {
   box-sizing: border-box;
 }
 
-.post {
-  padding: 15px;
-  margin-bottom: 10px;
-  border: 2px solid teal;
-}
-form {
-  display: flex;
-  flex-direction: column;
-}
-input {
-  width: 90%;
-  height: 40px;
-  border: 1px solid teal;
-  margin-top: 10px;
-  margin-bottom: 10px;
-  margin-left: 10px;
-  margin-right: 10px;
-  padding: 10px;
-}
-input:focus-visible {
-  border: 1px solid rgb(245, 67, 13);
-}
-input:focus {
-  border: 1px solid rgb(245, 67, 13);
-}
-button {
-  display: inline-block;
-  align-self: start;
-  margin-bottom: 10px;
-  margin-left: 10px;
-  padding: 10px;
-  line-height: 10px;
-  background-color: aquamarine;
-  border: none;
-  border-radius: 6px;
-  cursor: pointer;
-}
 </style>
---
+++ src/components/postForm.vue
@@ -0,0 +1,69 @@
+<template>
+  <div>
+    <form @submit.prevent>
+      <h4>Создание поста</h4>
+      <input
+        v-model="post.title"
+        class="input"
+        type="text"
+        placeholder="Название"
+      />
+      <input
+        v-model="post.title"
+        class="input"
+        type="text"
+        placeholder="Описание"
+      />
+      <button>Создать</button>
+      <button>Удалить</button>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      post: {
+        title: "",
+        body: ""
+      }
+    }
+  },
+};
+</script>
+
+<style  scoped>
+form {
+  display: flex;
+  flex-direction: column;
+}
+input {
+  width: 90%;
+  height: 40px;
+  border: 1px solid teal;
+  margin-top: 10px;
+  margin-bottom: 10px;
+  margin-left: 10px;
+  margin-right: 10px;
+  padding: 10px;
+}
+input:focus-visible {
+  border: 1px solid rgb(245, 67, 13);
+}
+input:focus {
+  border: 1px solid rgb(245, 67, 13);
+}
+button {
+  display: inline-block;
+  align-self: start;
+  margin-bottom: 10px;
+  margin-left: 10px;
+  padding: 10px;
+  line-height: 10px;
+  background-color: aquamarine;
+  border: none;
+  border-radius: 6px;
+  cursor: pointer;
+}
+</style>
